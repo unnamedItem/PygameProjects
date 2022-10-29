1ed69b3cced2d8503d79af514ec2fe1a345f281f
Cusmai Franco <fcusmai@b2gov.com>
2022-10-28 23:31:50 -0300

    deleted folders

--- templates/functions/utils.py
+++
@@ -1,9 +0,0 @@
-EXIT_STAGE = False
-
-def stage(func):
-    def wrapper():
-        print("Stage [", func.__name__, "] is running")
-        running: bool = True
-        while running or running == None:
-            running = func()
-    return wrapper
